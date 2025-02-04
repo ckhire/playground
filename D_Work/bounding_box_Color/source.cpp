7739e5fd0da9078389854f6030b8a1175e1a5070
Chaitanya Khire <60615631+ckhire@users.noreply.github.com>
2025-02-04 06:49:05 +0000

    First work with github copilot

--- D_Work/bounding_box_Color/source.cpp
+++ D_Work/bounding_box_Color/source.cpp
@@ -1,12 +1,12 @@
 #include<iostream>
 #include<opencv2/opencv.hpp>
 #include <vector>
-
+#include <cstdlib>
 using namespace std;
 using namespace cv;
 
 
-int main() {
+int main(int argc, char** argv) {
     
     int t1,t2, counter = 0;
     int img_w = 0;
@@ -15,7 +15,6 @@ int main() {
     int img_bb_h = 0;
     float r_perc = 0;
     float na_perc = 0;
-    void get_Pixel_Index(Mat);
     Vec3b *pixel = NULL;
     vector<int> R_Index;
     vector<int> N_Index;
@@ -23,8 +22,20 @@ int main() {
     int r_counter = 0;
     int n_counter = 0;
     
-    Mat img_src = imread("D:/Local_Development/03.ColorDetection/src/red_1.jpg", IMREAD_COLOR);
-    
+    if (argc < 3) {
+        std::cerr << "Usage: " << argv[0] << " <input_image_path> <output_image_path>" << std::endl;
+        return 1;
+    }
+    Mat img_src = imread(argv[1], IMREAD_COLOR);
+        Mat img = img_src(Range(2, 19), Range(3, 17));
+    } else {
+        std::cerr << "Image dimensions are too small for the specified ranges." << std::endl;
+        return -1;
+    }
+    if (img_src.empty()) {
+        std::cerr << "Could not open or find the image" << std::endl;
+        return 1;
+    }
     img_w = img_src.size().width;
     img_h = img_src.size().height;
 
@@ -36,7 +47,7 @@ int main() {
     img_bb_h = img_src.size().height;
 
     
-    std::cout << "Image Width : " << img_bb_w <<endl;
+    pixel = (Vec3b*)malloc(sizeof(Vec3b) * img.size().width * img.size().height);
     std::cout << "Image Height : " << img_bb_h <<endl;
     std::cout << "Image channels : " << img.channels()<<endl;
     
@@ -48,20 +59,20 @@ int main() {
     }
     else
         std::cout << "Pixels vec3b size is : " << sizeof(pixel)<<endl;
-
-    for (int x = 0; x < img.size().width; x++)
+        std::cout << "Pixels vec3b size is : " << (sizeof(Vec3b) * (img_bb_w * img_bb_h)) << endl;
+    for (int y = 0; y < img.size().height; y++)
     {
-        for (int y = 0; y < img.size().height; y++)
+        for (int x = 0; x < img.size().width; x++)
         {
             
-            pixel[counter] = img.at<Vec3b>(x, y);
+            pixel[counter] = img.at<Vec3b>(y, x);
 
-            if (img.at<Vec3b>(x, y)[2] >= 65)
+            if (img.at<Vec3b>(y, x)[2] >= 65)
             {
                 R_Index.insert(R_Index.end(),counter); 
                 r_counter++;
             }
-            else if (img.at<Vec3b>(x, y)[2] <= 65)
+            else if (img.at<Vec3b>(y, x)[2] <= 65)
             {
                 N_Index.insert(N_Index.end(), counter); 
                 n_counter++;
@@ -70,7 +81,7 @@ int main() {
         }
     }
 
-    for (int i = 1; i <= counter; i++)
+    for (int i = 0; i < counter; i++)
     {
         std::cout << "pixel[" << i << "] BGR values are : " << pixel[i]<<endl;
     }
@@ -94,19 +105,14 @@ int main() {
 
     r_perc = ((float)r_counter / (float)counter) * 100.0;
     std::cout << "Red colour perentage : " <<r_perc<< endl;
-    na_perc = ((float)n_counter / (float)counter) * 100.0;
+    imwrite(argv[2], img);
     std::cout << "Not Applicable colour perentage : " <<na_perc<< endl;
 
     t2 = getTickCount();
 
     imwrite("D:/Local_Development/03.ColorDetection/src/Out_put.jpg", img);
 
-    if(pixel != NULL)
-        free(pixel);
-
-  
-    img.release();
-    img_src.release();
-
+if(pixel != NULL)
+free(pixel);
     return 0;
 }
